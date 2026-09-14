d6b54348db39d2c59a563026f670c121ce65cf22
Vivek Jaiswal <vivekjais16@gmail.com>
2026-09-14 14:22:21 +0530

    chore: isolate kafka project to its standalone repository and remove from profile_project

--- projects/kafka_spark_streaming_pipeline/api/__init__.py
+++
@@ -1,4 +0,0 @@
-"""API package exports."""
-from .server import app
-
-__all__ = ["app"]
--- projects/kafka_spark_streaming_pipeline/producer/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Producer module exports."""
-from .generator import generate_synthetic_event, event_stream_generator
-from .async_producer import ResilientKafkaProducer
-
-__all__ = ["generate_synthetic_event", "event_stream_generator", "ResilientKafkaProducer"]
